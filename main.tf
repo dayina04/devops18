bff472e76faa5c60468029bb04886cacf81d61cf
dayina04 <sanjaydayina@gmail.com>
2026-06-12 14:52:13 +0530

    Update AMI and key name in launch template

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0cae6d6fe6048ca2c"
+    image_id           = "ami-0152204c1a187337c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "minimajor"
+    key_name = "firstkp"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-0d3ddecc67bc0c593", "subnet-0566cbf144fc97960"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
