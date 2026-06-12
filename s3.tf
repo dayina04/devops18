5749b6e6dc11e070bd3f07313192ae9ad79e4566
dayina04 <sanjaydayina@gmail.com>
2026-06-12 16:08:14 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,10 +22,10 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-/* terraform {
+terraform {
 backend "s3" {
 region = "us-east-1"
 bucket = "mustafashaik1122.monobucket"
-key = "prod/terraform.tfstate"
+key = "prod/storage2k26.tfstate"
 }
-} */
+} 
